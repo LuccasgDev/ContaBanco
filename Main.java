919a9fb91b6e934e405563f53d7ca88c1633bb29
LuccasgDev <163527503+LuccasgDev@users.noreply.github.com>
2024-12-13 17:53:50 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Criar uma instância de ContaTerminal
+        Contas contaTerminal = new Contas();
+
+        contaTerminal.coletarInformacoes();
+    }
+}
